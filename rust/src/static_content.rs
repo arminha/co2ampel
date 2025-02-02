b3d13f5c3c659ba9b0f52e08df550a644fea5cb8
Armin Häberling <armin.aha@gmail.com>
2025-02-02 21:04:08 +0100

    Add tests for StaticContent

--- rust/src/static_content.rs
+++ rust/src/static_content.rs
@@ -49,3 +49,73 @@ impl StaticContent {
         res
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::body::Bytes;
+
+    const CONTENT: &str = "Hello, world!";
+    const CONTENT_TYPE: &str = "text/plain";
+
+    #[test]
+    fn static_content_creation() {
+        let static_content = StaticContent::new(CONTENT, CONTENT_TYPE);
+
+        assert_eq!(static_content.content, CONTENT);
+        assert_eq!(static_content.content_type, CONTENT_TYPE);
+        assert_eq!(static_content.etag, compute_etag(CONTENT));
+    }
+
+    #[test]
+    fn get_request_with_matching_etag() {
+        let static_content = StaticContent::new(CONTENT, CONTENT_TYPE);
+        let if_none_match = IfNoneMatch::from(static_content.etag.clone());
+
+        let response = static_content.get_request(Some(if_none_match));
+
+        assert_eq!(response.status(), StatusCode::NOT_MODIFIED);
+        assert_eq!(
+            response.headers().typed_get::<ETag>(),
+            Some(static_content.etag)
+        );
+    }
+
+    #[tokio::test]
+    async fn get_request_with_non_matching_etag() {
+        let static_content = StaticContent::new(CONTENT, CONTENT_TYPE);
+        let non_matching_etag: ETag = "\"non-matching-etag\"".parse().unwrap();
+        let if_none_match: IfNoneMatch = non_matching_etag.into();
+
+        let response = static_content.get_request(Some(if_none_match));
+
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(response.headers().typed_get(), Some(static_content.etag));
+        assert_eq!(
+            response.headers().get(header::CONTENT_TYPE),
+            Some(&HeaderValue::from_static(CONTENT_TYPE))
+        );
+        assert_eq!(body_as_bytes(response).await, CONTENT);
+    }
+
+    #[tokio::test]
+    async fn get_request_without_if_none_match() {
+        let static_content = StaticContent::new(CONTENT, CONTENT_TYPE);
+
+        let response = static_content.get_request(None);
+
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(response.headers().typed_get(), Some(static_content.etag));
+        assert_eq!(
+            response.headers().get(header::CONTENT_TYPE),
+            Some(&HeaderValue::from_static(CONTENT_TYPE))
+        );
+        assert_eq!(body_as_bytes(response).await, CONTENT);
+    }
+
+    async fn body_as_bytes(response: Response<Body>) -> Bytes {
+        axum::body::to_bytes(response.into_parts().1, 1024)
+            .await
+            .unwrap()
+    }
+}
